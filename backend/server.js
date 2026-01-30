ddb3a582496b4ec9f47ae5d528ab4a7ae68d39c7
sushant <sushantmindi@gmail.com>
2026-01-30 22:18:28 +0530

    Add CORS support to backend

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,16 @@
 import jsonServer from "json-server";
+import cors from "cors";
 
 const server = jsonServer.create();
-const router = jsonServer.router("db.json");
+const router = jsonServer.router("db.json"); // or data.json (must exist)
 const middlewares = jsonServer.defaults();
 
 const PORT = process.env.PORT || 5000;
 
+server.use(cors());
 server.use(middlewares);
 server.use(jsonServer.bodyParser);
-server.use(router);
+server.use("/users", router);
 
 server.listen(PORT, () => {
     console.log("JSON Server running on port", PORT);
