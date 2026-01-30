d005f7cb9797194d548418dc8a52698dae970397
sushant <sushantmindi@gmail.com>
2026-01-30 23:19:24 +0530

    Json server commit 7

--- backend/server.js
+++ backend/server.js
@@ -2,24 +2,25 @@ import jsonServer from "json-server";
 import cors from "cors";
 
 const server = jsonServer.create();
-const router = jsonServer.router("db.json");
-const PORT = process.env.PORT || 10000;
+const router = jsonServer.router("./db.json");
+const middlewares = jsonServer.defaults();
 
+const PORT = process.env.PORT || 10000;
 server.use(
     cors({
         origin: "https://anime-store-react-app.netlify.app",
         methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-        allowedHeaders: ["Content-Type"],
-        credentials: false
+        allowedHeaders: ["Content-Type"]
     })
 );
 
 server.options("*", cors());
 
+server.use(middlewares);
 server.use(jsonServer.bodyParser);
 
 server.use(router);
 
 server.listen(PORT, () => {
-    console.log("JSON Server running on port", PORT);
+    console.log(`JSON Server running on port ${PORT}`);
 });
