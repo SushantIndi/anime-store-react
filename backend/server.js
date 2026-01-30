fcc7223b3b3cd3d707d62758e8e71947d22cf0c6
sushant <sushantmindi@gmail.com>
2026-01-30 22:46:56 +0530

    Json server commit

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 5000;
 server.use(cors());
 server.use(middlewares);
 server.use(jsonServer.bodyParser);
-server.use("/users", router);
+server.use(router);
 
 server.listen(PORT, () => {
     console.log("JSON Server running on port", PORT);
